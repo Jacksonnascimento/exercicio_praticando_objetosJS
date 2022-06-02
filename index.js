6f947f58cc457586e3c9b355635873abe735b465
Jacksonnascimento <jacksonnascimento84@hotmail.com>
2022-06-02 09:00:36 -0300

    modificações
    JS

--- index.js
+++ index.js
@@ -4,16 +4,16 @@ function addInfor(){
     nave.nome = prompt("Digite o nome da nave")
     nave.tipoNave = prompt("Digite o tipo da nave")
     nave.velocidadeMax = Number(prompt("Digite a velocidade máxima da nave")),
-    nave.acelear = 0
+    nave.acelerar = 0
 }
 
-function opcoesAceOuParar(){
+function menu(){
     var opcao 
     do{
         opcao = prompt("Você deseja acelerar ou parar? \nDigite 1 para acelear. \nDigite 2 para parar")
         switch (opcao) {
             case "1":
-                acelear()
+                acelerar()
                 break
             case "2":
                 infor()
@@ -25,11 +25,11 @@ function opcoesAceOuParar(){
     } while (opcao != "2")
 }
 
-function acelear(){
-        nave.acelear += Number(prompt("Digite a velocidade que deseja acelerar"))
-        console.log(nave.acelear)     
-    if(nave.acelear > nave.velocidadeMax){
-        alert("Velocidade  máxima ultrapassada" + "\nVelocidade da nave: " + nave.acelear + "km/s" + "\nVelocidade máxima: " + nave.velocidadeMax + "km/s")
+function acelerar(){
+        nave.acelerar += Number(prompt("Digite a velocidade que deseja acelerar"))
+        console.log(nave.acelerar)     
+    if(nave.acelerar > nave.velocidadeMax){
+        alert("Velocidade  máxima ultrapassada" + "\nVelocidade da nave: " + nave.acelerar + "km/s" + "\nVelocidade máxima: " + nave.velocidadeMax + "km/s")
     }
     
 }
@@ -37,9 +37,10 @@ function acelear(){
 function infor (){
     var infor = "Nome da nave: " + nave.nome
     infor += "\n Tipo de nave: " + nave.tipoNave
-    infor += "\n Velocidade da nave: " + nave.acelear + " km/s"
+    infor += "\n Velocidade da nave: " + nave.acelerar + " km/s"
     console.log(infor)
+    alert(infor)
 }
 
 addInfor()
-opcoesAceOuParar()
+menu()
